5e0720ea67660eef5db740b0e9f362c9b7e391de
Mohammad Yosefpor <myusefpur@gmail.com>
2022-08-04 15:36:23 +0430

    change to generic

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/m-yosefpor/utils
 
-go 1.16
+go 1.18
--- utils.go
+++ utils.go
@@ -1,7 +1,7 @@
 package utils
 
 // isIn checks if a string array contains an element
-func IsIn(s string, list []string) bool {
+func IsIn[T comparable](s T, list []T) bool {
 	for _, l := range list {
 		if s == l {
 			return true
@@ -11,12 +11,12 @@ func IsIn(s string, list []string) bool {
 }
 
 // difference returns the elements in `a` that aren't in `b`.
-func ListDifference(a, b []string) []string {
-	mb := make(map[string]struct{}, len(b))
+func ListDifference[T comparable](a, b []T) []T {
+	mb := make(map[T]struct{}, len(b))
 	for _, x := range b {
 		mb[x] = struct{}{}
 	}
-	var diff []string
+	var diff []T
 	for _, x := range a {
 		if _, found := mb[x]; !found {
 			diff = append(diff, x)
